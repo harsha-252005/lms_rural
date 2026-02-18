75f8eee5ca6dbf8ad53ccc4b0831693b66204388
HariniPandiraju <harinipandiraju07@gmail.com>
2026-02-18 10:30:19 +0530

    lessons added

---
+++ src/main/java/com/example/demo/repository/LessonRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Lesson;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface LessonRepository extends JpaRepository<Lesson, Long> {
+    List<Lesson> findByCourseIdOrderByOrderIndexAsc(Long courseId);
+}
